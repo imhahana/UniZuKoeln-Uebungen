696b615ad706765a67640009dce4f9d7341b374a
nguye <nguye@HaHana2540>
2026-01-18 23:48:36 +0100

    schon mit Lösung korrigiert

--- Uebung02/src/hausaufgabe1/If_Else_Bedingte_Subtraktion.java
+++ Uebung02/src/hausaufgabe1/If_Else_Bedingte_Subtraktion.java
@@ -3,7 +3,7 @@ package hausaufgabe1;
 public class If_Else_Bedingte_Subtraktion {
 	public static void main (String [] args) {
 	
-		int z = 5;
+		int z = -11;
 	
 		if (z > 10) {
 			System.out.println(z - 10);
--- Uebung02/src/hausaufgabe2/MinimaMaxinma.java
+++ Uebung02/src/hausaufgabe2/MinimaMaxinma.java
@@ -4,12 +4,12 @@ public class MinimaMaxinma {
 
 	public static void main(String[] args) {
 		
-		int i = 1823;
-		int j = 321;
-		int k = 13723;
+		int i = -1;
+		int j = -2;
+		int k = -3;
 		
-		int min;
-		int max;
+		int min = 0;
+		int max = -2;
 		
 	//Find Maxima
 		if (i > j  &&  i > k) {
@@ -40,12 +40,14 @@ public class MinimaMaxinma {
 
 }
 
+//Chấm điểm dựa vào Testfälle:
+//-1: vergessen, min & max einen beliebigen Wert zuzuweisen
 
 //This version is the best one that I made, which was corrected by ChatGPT
 //1.Problem: i didn't use the && to combine method (logically mistake)
 //2.Problem: Assuming all values are different (what if they're the same => cannot print)
 //3.Problem: I cannot "den minimalen Wert an min zuweisen" & "den maximalen Wert an max zuweisen"
-//4. Problem: The task want me to find min (max) first than print. Not at the same time
-//5. Problem: When typing ein beliebige Nummer, shouldn't type 07 instead of 7 => In Java, numbers with a leading 0 are interpreted as octal (base-8)
+//4.Problem: The task want me to find min (max) first than print. Not at the same time
+//5.Problem: When typing ein beliebige Nummer, shouldn't type 07 instead of 7 => In Java, numbers with a leading 0 are interpreted as octal (base-8)
 
 //Explanation of "den minimalen Wert an min zuweisen": Store the smallest number in the variable min (Not print it, but Store it in memory)
