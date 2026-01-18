030d14941c9172f56f35f0d8513033009a13b8ef
nguye <nguye@HaHana2540>
2026-01-18 23:33:21 +0100

    finish Uebung 1

--- Uebung02/src/hausaufgabe3/If_Else.java
+++ Uebung02/src/hausaufgabe3/If_Else.java
@@ -1,10 +1,36 @@
 package hausaufgabe3;
 
 public class If_Else {
-	
-	int i, j, k;
-	i = 3;
-	j = 7;
-	k = 10;
+	public static void main (String[] args) {
+		
+		int i, j, k;
+		i = -3;
+		j = 10;
+		k = 100;
+		
+		if (i > j  &&  i > 200  &&  j > 100) {
+			k = 3;
+			
+		} else if (i > 200  &&  j > 100) {
+			k = -10;
+			
+		} else if (i > j  &&  i > 200) {
+			k = 2;
+			
+		} else if (i > j) {
+			k = 1;
+			
+		} else if (i > 200) {
+			k = -10;
+			
+		} else if (j > 100) {
+			k = -10;
+			
+		} else {
+			k = 4;
+		}
+		
+		System.out.println(k);
+	}
 
 }
